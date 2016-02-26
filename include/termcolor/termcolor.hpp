e4f1326e2bbd15718fd7c02c998b21d4ee90abf0
Waldemar Villamayor-Venialbo <wvenialbo@gmail.com>
2016-02-26 09:02:06 -0300

    Added support for BSDs and generic UNIX platforms.
    Signed-off-by: Waldemar Villamayor-Venialbo <wvenialbo@gmail.com>

--- include/termcolor/termcolor.hpp
+++ include/termcolor/termcolor.hpp
@@ -19,7 +19,7 @@
 #   define OS_WINDOWS
 #elif defined(__APPLE__)
 #   define OS_MACOS
-#elif defined(linux) || defined(__linux) || defined(__CYGWIN__)
+#elif defined(linux) || defined(__linux) || defined(unix) || defined(__unix) || defined(__CYGWIN__)
 #   define OS_LINUX
 #else
 #   error unsupported platform

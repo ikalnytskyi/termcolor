57377b332ba954ac64960567bd61efa194914e2d
Curt Martin <trailfrenzy@gmail.com>
2020-08-24 09:38:42 -0700

    Update termcolor.hpp

--- include/termcolor/termcolor.hpp
+++ include/termcolor/termcolor.hpp
@@ -136,7 +136,8 @@ namespace termcolor
         #if defined(TERMCOLOR_OS_MACOS) || defined(TERMCOLOR_OS_LINUX)
             stream << "\033[4m";
         #elif defined(TERMCOLOR_OS_WINDOWS)
-        #endif
+			_internal::win_change_attributes(stream, -1, COMMON_LVB_UNDERSCORE);
+		#endif
         }
         return stream;
     }
